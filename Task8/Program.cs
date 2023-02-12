473db63d61702aec98699e11bacfe9df3aaed932
Nick <nick00712@yandex.ru>
2023-02-13 00:35:44 +0500

    задание 8 выполнено для положительных и отрицательных чисел

---
+++ Task8/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+if (N > 0)
+{
+
+  if (N < 2)
+  {
+    Console.Write("Четных чисел нет");
+    count = N;
+  }
+  else
+  {
+    while (count < N - 1)
+    {
+      count = count + 2;
+      Console.Write(count + " ");
+    }
+  }
+
+}
+else
+{
+
+  if (N > -2)
+  {
+    Console.Write("Четных чисел нет");
+    count = N;
+  }
+  else
+  {
+    while (count > N + 1)
+    {
+      count = count - 2;
+      Console.Write(count + " ");
+    }
+  }
+
+}
