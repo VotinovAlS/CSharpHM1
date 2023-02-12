ef3f2c84d8a2a9fb5ed919d6592f402168fcb986
Nick <nick00712@yandex.ru>
2023-02-13 00:11:33 +0500

    задача 6 выполнена

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Для проверки числа на четность введите его: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0)
+{
+  Console.WriteLine("да");
+}
+else
+{
+  Console.WriteLine("нет");
+}
